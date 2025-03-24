dd39c3d049a01907e01f280f72e77af79a75f4eb
thief <thiefainedev@gmail.com>
2025-03-24 19:04:58 +0100

    fix(cv): update cv

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -4,8 +4,8 @@ import PageTitle from "@/components/PageTitle/pageTitle";
 import { Download } from "lucide-react";
 import Image from "next/image";
 export default async function CV() {
-  const urlCvImage = "/doc/CV_SIMONNOU-Thiefaine.webp";
-  const urlCv = "/doc/CV_SIMONNOU-Thiefaine.pdf";
+  const urlCvImage = "/doc/CV_Simonnou-Thiefaine_developpeur.png";
+  const urlCv = "/doc/CV_Simonnou-Thiefaine_developpeur.pdf";
   return (
     <>
       <PageTitle>CV</PageTitle>
